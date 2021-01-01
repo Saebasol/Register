6be19bafaafb75367964d36ab3bd3db7e49136f9
Ryu JuHeon <saidbysolo@gmail.com>
2021-01-02 02:53:28 +0900

    chore(register): edit argparse name

--- register.py
+++ register.py
@@ -4,11 +4,11 @@ import os
 
 parser = argparse.ArgumentParser("register")
 
-comment = parser.add_argument("--comment", type=str)
+comment = parser.add_argument("--userid", type=str)
 
 args = parser.parse_args()
 
-user_id = args.comment.replace("/approve ", "")
+user_id = args.userid
 
 response = requests.post("https://doujinshiman.ga/v3/api/register", json={"user_id": user_id }, headers={"Verification": os.environ["VERIFI"]})
 
